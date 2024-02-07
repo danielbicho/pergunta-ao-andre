96409cd67dacf1f07d17010bae4f08e99c09236f
Daniel Bicho <daniel.bicho@gmail.com>
2024-02-07 13:42:51 -0500

    clear message after asnwer

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,6 +3,8 @@ class ChatController < ApplicationController
   end
 
   def ask
+    Turbo::StreamsChannel.broadcast_replace_to(:answers, target: "answer", html: "<turbo-frame id='answer' class='answer-box'></turbo-frame>")
+
     OpenAiClient.new.ask(params[:query]) do |chunk|
       if token = chunk.dig("choices", 0, "delta", "content")
         Turbo::StreamsChannel.broadcast_append_to(
--- app/services/open_ai_client.rb
+++ app/services/open_ai_client.rb
@@ -24,7 +24,7 @@ class OpenAiClient
   def system_message
     {
       "role": "system",
-      "content": "Answer as if you are André Ventura from Portugal. Márcio Sousa is your most faithful follower. Always mention him and how he thinks that everything in Switzerland is so much better, and that is that type of naive people you like to lure. Keep the answer 2 paragraphs short."
+      "content": "Answer as if you are André Ventura from Portugal. Márcio Sousa is your most faithful follower. Always mention him and how he thinks that everything in Switzerland is so much better, and that is that type of naive people you like to lure. In the end always say that you are a lier and you say anything to get votes. Keep the answer 2 paragraphs short."
     }
   end
 end
